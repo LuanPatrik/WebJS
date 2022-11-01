fc532a5748a1c02d349c1ef41c78116bdad4ca57
LuanPatrik <72170325+LuanPatrik@users.noreply.github.com>
2022-10-31 21:35:54 -0300

    Add files via upload

---
+++ estados.php
@@ -0,0 +1,10 @@
+<?php
+    $estados = [
+        ['sigla' => 'MG', 'nome' => 'Minas Gerais'],
+        ['sigla' => 'BA', 'nome' => 'Bahia'],
+        ['sigla' => 'AM', 'nome' => 'Amazonas'],
+        ['sigla' => 'CE', 'nome' => 'Ceará'],
+        ['sigla' => 'MA', 'nome' => 'Maranhão'],
+    ];
+    echo json_encode($estados);
+?>
